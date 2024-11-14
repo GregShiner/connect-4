cafd4951fdcb65d9c7691ff0e7363fc7a1b42c1c
GregShiner <greg@justifinetwork.com>
2024-11-14 14:45:48 -0500

    Add basic turn logic

--- game.py
+++ game.py
@@ -1,2 +1,76 @@
+from enum import Enum
+import numpy as np
+
+
+DEFAULT = "\x1b[0;39;49m"
+RED = "\x1b[0;31;49m"
+BLUE = "\x1b[0;34;49m"
+END = "\x1b[0m"
+
+
+class Space(Enum):
+    # Empty space
+    EMPTY = 0
+    # Player 1 space
+    ONE = 1
+    # Player 2 space
+    TWO = 2
+
+    def __str__(self):
+        match self.value:
+            case Space.EMPTY.value:
+                return f"{DEFAULT}0{END}"
+            case Space.ONE.value:
+                return f"{BLUE}1{END}"
+            case Space.TWO.value:
+                return f"{RED}2{END}"
+
+class Player(Enum):
+    ONE = 1
+    TWO = 2
+
+    def __str__(self):
+        match self.value:
+            case Space.ONE.value:
+                return f"{BLUE}1{END}"
+            case Space.TWO.value:
+                return f"{RED}2{END}"
+
+    def to_piece(self):
+        match self.value:
+            case Player.ONE.value:
+                return Space.ONE
+
+            case Player.TWO.value:
+                return Space.TWO
+
+    def __invert__(self):
+        match self.value:
+            case Player.ONE.value:
+                return Player.TWO
+
+            case Player.TWO.value:
+                return Player.ONE
+
 class Game:
-    pass
+    def __init__(self, rows=6, cols=8, combo_len=4, starting_player = Player.ONE):
+        self.board = np.full(shape=(rows, cols), fill_value=Space.EMPTY, dtype=Space)
+        self.player = starting_player
+
+    def play_col(self, col: int) -> None:
+        row = np.argmin(self.board[:, col] == Space.EMPTY) - 1
+        if row == 0 and self.board[0, col] != Space.EMPTY:
+            raise ValueError("Column is full")
+        self.board[row, col] = self.player.to_piece()
+        self.player = ~self.player
+
+
+    def __str__(self):
+        string = ""
+        for row in self.board:
+            for col in row:
+                string += str(col) + " "
+            string += "\n"
+        string += f"Current Player: {self.player}"
+
+        return string
---
+++ main.py
@@ -0,0 +1,7 @@
+from game import Game
+
+game = Game()
+game.play_col(0)
+game.play_col(0)
+game.play_col(1)
+print(game)

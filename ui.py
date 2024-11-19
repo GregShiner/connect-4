c40641d4087c9fe444d49e0bf75b0129829a4868
gregs <gregsalisbury25@gmail.com>
2024-11-18 22:01:46 -0500

    added game screen and board, switched to Frames

--- ui.py
+++ ui.py
@@ -17,9 +17,10 @@ root = tk.Tk()
 #     root.grid_columnconfigure(j, weight=1)
 
 root.title('Connect 4')
+root.geometry('1000x1000')
 
-canvas = tk.Canvas(root, width=800, height=600, bg='black')
-
-canvas.pack()
-
+game_screen = tk.Frame(root, width=800, height=600, bg='white')
+game_screen.place(x=100, y=100)
+game_board = tk.Frame(root, width=600, height=450, bg='black')
+game_board.place(x=200, y=175)
 root.mainloop()

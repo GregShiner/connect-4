c30824d0729e542d186c61cdcf75c5c648dcc73f
Greg Shiner <greg@gregshiner.com>
2024-12-08 19:18:16 -0500

    WIP: win checking

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from enum import Enum
 import numpy as np
+from typing import Tuple
 
 
 DEFAULT = "\x1b[0;39;49m"
@@ -54,16 +55,64 @@ class Player(Enum):
 
 class Game:
     def __init__(self, rows=6, cols=8, combo_len=4, starting_player = Player.ONE):
-        self.board = np.full(shape=(rows, cols), fill_value=Space.EMPTY, dtype=Space)
+        self.rows = rows
+        self.cols = cols
+        self.combo_len = combo_len
+        self.board = np.full(shape=(self.rows, self.cols), fill_value=Space.EMPTY, dtype=Space)
         self.player = starting_player
 
-    def play_col(self, col: int) -> None:
+    def play_col(self, col: int) -> bool:
+        # Row gets set to -1 in 2 conditions
+        # 1. Its the first row
+        # 2. There is no available slot
         row = np.argmin(self.board[:, col] == Space.EMPTY) - 1
-        if row == 0 and self.board[0, col] != Space.EMPTY:
+        if row == -1 and self.board[0, col] != Space.EMPTY:
             raise ValueError("Column is full")
+        # The row being set to -1 to represent the last cell breaks the win checking logic, so set it back to a positive index
+        if row == -1:
+            row = self.rows - 1
         self.board[row, col] = self.player.to_piece()
+        is_win = self.check_win((row, col))
+        if is_win:
+            return True
         self.player = ~self.player
+        return False
 
+    def check_win(self, coords: Tuple[int, int]) -> bool:
+        number_in_row = 0
+        # for row_mult, col_mult in [(0, 1), (1, 0), (1, 1), (1, -1)]:
+        #     for offset in range(-(self.combo_len - 1), self.combo_len):
+        #         row = (row_mult * offset) + coords[0]
+        #         col = (row_mult * offset) + coords[1]
+        #
+        #         if row < 0 or row >= self.rows:
+        #             continue
+        #         if col < 0 or col >= self.cols:
+        #             continue
+        #
+        #         if self.board[row, col] == self.player.to_piece():
+        #             number_in_row += 1
+        #         else:
+        #             number_in_row = 0
+        #
+        #         if number_in_row == 4:
+        #             return True
+        for offset in range(-(self.combo_len - 1), self.combo_len):
+            row = offset + coords[0]
+            col = coords[1]
+
+            if row < 0 or row >= self.rows:
+                continue
+            if col < 0 or col >= self.cols:
+                continue
+
+            if self.board[row, col] == self.player.to_piece():
+                number_in_row += 1
+            else:
+                number_in_row = 0
+
+            if number_in_row == 4:
+                return True
 
     def __str__(self):
         string = ""
